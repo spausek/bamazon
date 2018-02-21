6322a94e4d29885d73dd4eeff84790f489db6538
Kyle Cohen <spausek@gmail.com>
2018-02-21 13:44:00 -0800

    Made the table out put look prettier and easier to read with a for loop

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,12 +14,17 @@ function start(){
 		if (error) {
 			console.error(error);
 		}
-		console.log(data);
 		console.log('Welcome to Bamazon\'s Adventure Emporium serving all your RPG needs!')
+
+		for (var i = 0; i < data.length; i++) {
+			console.log(' ID ' + data[i].id + ' Item ' + data[i].product_name + ' Department ' + data[i].department_name + ' Price ' + data[i].price + ' Stock ' + data[i].stock);
+			
+		}
 	
+		//console.log(data);
+		
      
-inquirer
-.prompt([
+inquirer.prompt([
 		{
 			name: 'item',
        		type: 'number',
@@ -34,7 +39,7 @@ inquirer
 ]) 
 
 .then(function(answer){
-	var purchase = answer.item;
+	var purchase = (answer.item)-1;
 	var amount = answer.amountToPurchase;
 	var total = parseFloat(((data[purchase].price)*amount).toFixed(2));
 
@@ -44,16 +49,40 @@ inquirer
 		{id:answer.item}
 		], function (err, result){
 			if(err) throw err;
-			console.log('Purchase Made! Your total is $' + total.toFixed(2) + '. Your items(s) will be shipped to a location of your choice in a few days!')
+			
 		});
+		console.log('Purchase Made! Your total is $' + total.toFixed(2) + '. Your items(s) will be shipped to a location of your choice in a few days!')
 	   }
 
 	   else{
 	   	console.log('Sorry we don\'t have enough in our inventory =(');
 	   }
+
+	   restart();
+
 })
 
 });
+
+}
+
+function restart(){
+	inquirer.prompt(
+	{
+		name: 'buyMore',
+		type: 'confirm',
+		message: 'Would you like to make another purchase?'
+
+	})
+	.then(function(answer){
+		if(answer.buymore){
+			start();
+		}
+
+		else{
+			console.log('Keep grinding and see us again when you level up!');
+		}
+	});
 
 }
 
